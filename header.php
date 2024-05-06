8bf10ab85bd389a60fdc3d591a38b5c3c1859e5f
Nguyen Duc An <annd.oneadx@gmail.com>
2024-05-06 17:43:48 +0700

    add product

--- header.php
+++ header.php
@@ -2,8 +2,8 @@
     <div class="header_body">
     <a href="index.php" class="logo">TestPHP</a>
     <nav class="navbar">
-        <a href=""> Add Products</a>
-        <a href="">View Products</a>
+        <a href="index.php"> Add Products</a>
+        <a href="view_products.php">View Products</a>
         <a href="">Shopit</a>
     </nav>
     <a href="" class="cart"><i class="fa-solid fa-cart-shopping"></i><span><sup>4</sup></span></a>
--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<?php include 'connect.php' ;
+if(isset($_POST['add_product'])){
+    $product_name = $_POST['product_name'];
+    $product_price = $_POST['product_price'];
+    $product_image = $_FILES['product_image']['name'];
+    $product_image_temp_name = $_FILES['product_image']['tmp_name'];
+    $product_image_folder = 'images/'.$product_image;
+
+    $insert_query=mysqli_query($conn, "insert into `products` (name,price,image) values
+    ('$product_name','$product_price', '$product_image' )") or die("Insert query failed");
+    if($insert_query) {
+        move_uploaded_file($product_image_temp_name,$product_image_folder);
+        $display_message =  "Product inserted successfully";
+    } else {
+        $display_message =  "There is some error inserting product";
+    }
+}   
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,16 +30,25 @@
 <body>
 
 <?php include('header.php') ?>
-<?php include('connect.php') ?>
+
 <!-- form -->
 <div class="container">
+    <?php 
+    if(isset($display_message)) {
+        echo   "<div class='display_message'>
+        <span>$display_message</span>
+        <i class='fas fa-times' onclick='this.parentElement.style.display=`none`';></i>
+    </div>";
+    }
+    ?>
+
     <section>
         <h3 class="heading">Add Products</h3>
-        <form action="" class="add_product" >
-            <input type="text" placeholder="Enter product name" class="input_fields" required >
-            <input type="number" placeholder="Enter product price" class="input_fields" required >
-            <input type="file" class="input_fields" required >
-            <input type="submit" class="submit_btn" value="Add Product" >
+        <form action="" class="add_product" method="post" enctype="multipart/form-data">
+            <input type="text" name="product_name" placeholder="Enter product name" class="input_fields" required >
+            <input type="number" name="product_price" min="0" placeholder="Enter product price" class="input_fields" required >
+            <input type="file" name="product_image" class="input_fields" required accept="image/png, image/jpg, image/jpeg">
+            <input type="submit" name="add_product" class="submit_btn" value="Add Product" >
         </form>
     </section>
 </div>
